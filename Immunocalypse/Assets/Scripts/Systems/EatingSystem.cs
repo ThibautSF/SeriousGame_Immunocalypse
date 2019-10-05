d32e840e62a88a68914486a6f573bb7a96366c60
ThibautSF <tsimonfine@gmail.com>
2019-10-05 17:09:38 +0200

    Prefabs and Textures
    - Add basic file structure
    - Add textures (bacteria, virus, immune cells...)
    - Add virus and bacteria prefab sample

---
+++ Immunocalypse/Assets/Scripts/Systems/EatingSystem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using FYFY;
+using FYFY_plugins.TriggerManager;
+
+public class EatingSystem : FSystem {
+	private Family _triggeredGO = FamilyManager.getFamily(
+		new AllOfComponents(typeof(Triggered2D))
+	);
+
+	// Use to process your families.
+	protected override void onProcess(int familiesUpdateCount) {
+		foreach (GameObject go in _triggeredGO) {
+			Triggered2D t2d = go.GetComponent<Triggered2D>();
+
+			foreach (GameObject target in t2d.Targets) {
+				GameObjectManager.unbind(target);
+				Object.Destroy(target);
+			}
+		}
+	}
+}
