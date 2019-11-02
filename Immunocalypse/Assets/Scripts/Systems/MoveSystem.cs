7e059abd0cb4ed8fd74926f0095aa3372a67d420
ThibautSF <tsimonfine@gmail.com>
2019-11-02 23:55:58 +0100

    Move modifiers tilemap based

--- Immunocalypse/Assets/Scripts/Systems/MoveSystem.cs
+++ Immunocalypse/Assets/Scripts/Systems/MoveSystem.cs
@@ -14,7 +14,7 @@ public class MoveSystem : FSystem {
 		Grid grid = Object.FindObjectOfType<Grid>();
 
 		if (grid != null) {
-			myMaps.GetComponent<GridMap>();
+			myMaps = grid.GetComponent<GridMap>();
 		}
 	}
 
@@ -24,24 +24,18 @@ public class MoveSystem : FSystem {
 			Transform tr = go.GetComponent<Transform>();
 			Move mv = go.GetComponent<Move>();
 
-			Vector3 movement = Vector3.zero;
-
-			tr.position = Vector3.MoveTowards(tr.position, mv.targetPosition, mv.speed * Time.deltaTime);
-
-			/*
-			Vector3 newDir = Vector3.RotateTowards(tr.forward, mv.target, mv.speed * Time.deltaTime, 0.0f);
-			newDir = new Vector3(newDir.x, 0, newDir.z);
-			Debug.DrawRay(tr.position, newDir, Color.red);
-			tr.rotation = Quaternion.LookRotation(newDir);
-			*/
-
-			Vector3 vectorToTarget = mv.targetPosition - tr.position;
+			Vector2 movement = Vector2.zero;
+			
+			//Look at direction
+			Vector2 vectorToTarget = mv.targetPosition - tr.position;
 			Debug.DrawRay(tr.position, vectorToTarget, Color.red);
-
 			float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
 			Quaternion qt = Quaternion.AngleAxis(angle, Vector3.forward);
 			tr.rotation = Quaternion.RotateTowards(tr.rotation, qt, Time.deltaTime * mv.speed * 100);
-
+			
+			//Move
+			float modifiers = getSpeedModifier(tr.position);
+			tr.position = Vector2.MoveTowards(tr.position, mv.targetPosition, mv.speed * modifiers * Time.deltaTime);
 		}
 	}
 
@@ -51,5 +45,18 @@ public class MoveSystem : FSystem {
 
 	private bool hasTile(Tilemap tilemap, Vector2 worldPos) {
 		return tilemap.HasTile(tilemap.WorldToCell(worldPos));
+	}
+
+	private float getSpeedModifier(Vector2 worldPos) {
+		float sm = 1f;
+
+		for (int i = myMaps.myTileMaps.Count - 1; i >= 0 ; i--) {
+			if (hasTile(myMaps.myTileMaps[i], worldPos)) {
+				sm *= myMaps.myTileMaps[i].GetComponent<MapLayer>().speedBonus;
+				break;
+			}
+		}
+
+		return sm;
 	}
 }
